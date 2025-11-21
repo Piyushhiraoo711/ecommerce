9adcb4b729d11b3d146093f4ea5e916705c48341
piyush hiraoo <pihiraoo@bestpeers.in>
2025-11-21 17:40:23 +0530

    fix issue

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,5 +1,5 @@
-export const USER_API_END_POINT="http://localhost:5000/auth";
-export const PRODUCT_API_END_POINT="http://localhost:5000/product";
-export const ADMIN_API_END_POINT="http://localhost:5000/admin";
-export const ORDER_API_END_POINT="http://localhost:5000/order";
-export const CART_API_END_POINT="http://localhost:5000/cart"
+export const USER_API_END_POINT="https://ecommerce-7yhu.onrender.com/auth";
+export const PRODUCT_API_END_POINT="https://ecommerce-7yhu.onrender.com/product";
+export const ADMIN_API_END_POINT="https://ecommerce-7yhu.onrender.com/admin";
+export const ORDER_API_END_POINT="https://ecommerce-7yhu.onrender.com/order";
+export const CART_API_END_POINT="https://ecommerce-7yhu.onrender.com/cart";
